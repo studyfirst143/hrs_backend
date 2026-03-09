9cb8814bb677868075ce37865a45b20b9e47f8de
jude villarante <judevillarante8@gmail.com>
2026-03-09 22:56:00 +0800

    adding code in delele from the reservation contoller

--- src/controllers/reservationController.ts
+++ src/controllers/reservationController.ts
@@ -167,6 +167,8 @@ export const deleteReservation = async (req: Request, res: Response) => {
     const reservation = await Reservation.findByIdAndDelete(req.params.id);
     if (!reservation) return res.status(404).json({ message: "Reservation not found" });
 
+  await Room.findByIdAndUpdate(reservation.roomId, { status: "available" });
+  
     res.json({ message: "Reservation deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Failed to delete reservation", error });
